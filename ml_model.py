c575ec184855a2af3612245f3f86c6b711879f35
imamanmehrotra <amansammehrotra@gmail.com>
2023-06-10 21:35:32 +0530

    Commiting after adding more models

--- ml_model.py
+++ ml_model.py
@@ -1,1 +1,18 @@
 print("Adding all ML related code")
+
+
+def model1(a,b):
+    print("Model1 has acc: 80%")
+
+
+
+def model2(a,b):
+    print("Model1 has acc: 90%")
+
+
+
+def model3(a,b):
+    print("Model1 has acc: 70%")
+
+a = 10
+b = 5
